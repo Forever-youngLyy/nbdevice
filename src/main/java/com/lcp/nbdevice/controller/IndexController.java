ba75e003fca8071d1c428032b518a56ae4738e08
Forever-youngLyy <1321649726@qq.com>
2020-11-10 14:41:11 +0800

    Update Project From Fork By LCP

--- src/main/java/com/lcp/nbdevice/controller/IndexController.java
+++ src/main/java/com/lcp/nbdevice/controller/IndexController.java
@@ -3,9 +3,7 @@ package com.lcp.nbdevice.controller;
 import org.springframework.boot.system.ApplicationHome;
 import org.springframework.web.bind.annotation.*;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
+import java.io.*;
 import java.util.Base64;
 import java.util.Map;
 
@@ -42,19 +40,43 @@ public class IndexController {
         return "{\"msg\":\"OK\"}";
     }
 
-    @GetMapping({"/",""})
-    public Object getBb(){
+    @GetMapping({"/", ""})
+    public Object getBb() {
+        int i = 0;
+        String ret = "";
+        int j = 0;
+        int cmd = 0;
         StringBuilder s = new StringBuilder();
-        try {
-            FileInputStream is = new FileInputStream(new File(path + fileName));
-            byte[] bytes = new byte[1024];
-            int n = 0;
-            while ((n = is.read(bytes)) != -1){
-                s.append(new String(bytes));
-            }
+        int lenth = 0;
+        try (InputStream is = new BufferedInputStream(new FileInputStream(new File(path + fileName)))) {
+            int len = is.available();
+            byte[] bytes = new byte[len];
+            is.read(bytes, 0, len);
+            s.append(new String(bytes));
         } catch (Throwable e) {
             e.printStackTrace();
         }
-        return s.toString();
+
+        for (; i < s.toString().length() - 31; i++) {
+            if (s.charAt(i) == 0x68) {
+                cmd = (int) ((s.charAt(i + 26) << 8) + s.charAt(i + 27));
+                switch (cmd) {
+                    case 0x0A01:
+                        lenth = s.charAt(i + 30);
+                        i += 31;
+                        ret += "<p>" + String.format("数据%d条   \n", lenth);
+                        for (j = 0; j < lenth && i < s.toString().length() - 7; j++) {
+                            ret = ret + String.format("%s 时间:%02x-%02x-%02x %02x:%02x:%02x", s.charAt(i + 1) > 0 ? "B->A" : "A->B", (int) s.charAt(i + 2),
+                                    (int) s.charAt(i + 3), (int) s.charAt(i + 4), (int) s.charAt(i + 5), (int) s.charAt(i + 6), (int) s.charAt(i + 7)) + "\n";
+                            i += 8;
+                        }
+                        ret += "</p>";
+                        break;
+                    case 0x0A02:
+                        break;
+                }
+            }
+        }
+        return ret;
     }
 }
